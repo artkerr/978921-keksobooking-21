a49f239ac15b294c218d3f031b5f648b182c9ff7
Denis <d.y.vladimirov@gmail.com>
2020-10-04 13:53:24 +0300

    Правки линтера

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const OFFER_TITLE = ['Апартаменты-студия', 'Аппартаменты в центре', 'Дом за городом', 'Малокомнатная квартира', 'Многокомнатная квартира', 'Несколькокомнатная квартира'];
-const OFFER_TYPE = ['palace', 'flat', 'house', 'bungalow'];
-const OFFER_CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
-const OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const OFFER_TITLE = [`Апартаменты-студия`, `Аппартаменты в центре`, `Дом за городом`, `Малокомнатная квартира`, `Многокомнатная квартира`, `Несколькокомнатная квартира`];
+const OFFER_TYPE = [`palace`, `flat`, `house`, `bungalow`];
+const OFFER_CHECKIN_CHECKOUT = [`12:00`, `13:00`, `14:00`];
+const OFFER_FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const OFFER_DESCRIPTION = [
-  'Новостройка расположена в Ценратральном районе, на территории национального парка.',
-  'Охраняемая территория, система контроля управления доступом в т.ч. по смартфону. Есть подземный паркинг.',
-  'Собственная инфраструктура: детский сад, детская площадка, тир, тренажерный зал, бассейн, спортивно-оздоровительный комплекс, собственное футбольное поле, магазины, салоны красоты, сауны, массажный и косметический салоны и т.д.',
-  'Очень престижный район, инфраструктура мегаполиса и тихий двор, большой плюс, что 2 из 3х балконов находятся в данных комнатах.'];
+  `Новостройка расположена в Ценратральном районе, на территории национального парка.`,
+  `Охраняемая территория, система контроля управления доступом в т.ч. по смартфону. Есть подземный паркинг.`,
+  `Собственная инфраструктура: детский сад, детская площадка, тир, тренажерный зал, бассейн, спортивно-оздоровительный комплекс, собственное футбольное поле, магазины, салоны красоты, сауны, массажный и косметический салоны и т.д.`,
+  `Очень престижный район, инфраструктура мегаполиса и тихий двор, большой плюс, что 2 из 3х балконов находятся в данных комнатах.`];
 const OFFER_PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
 const ADVERT_QUANTITY = 8;
 const advertList = [];
@@ -50,20 +50,20 @@ for (let i = 0; i < ADVERT_QUANTITY; i++) {
   advertList.push(getAdvert());
 }
 
-const activeMap = document.querySelector('.map');
-activeMap.classList.remove('map--faded');
-const pinsList = document.querySelector('.map__pins');
-const advertTemplate = document.querySelector('#pin').content;
+const activeMap = document.querySelector(`.map`);
+activeMap.classList.remove(`map--faded`);
+const pinsList = document.querySelector(`.map__pins`);
+const advertTemplate = document.querySelector(`#pin`).content;
 
 
 const createPin = (data) => {
   const pinElement = advertTemplate.cloneNode(true);
-  const pinButton = pinElement.querySelector('.map__pin');
-  const pinPhoto = pinElement.querySelector('img');
+  const pinButton = pinElement.querySelector(`.map__pin`);
+  const pinPhoto = pinElement.querySelector(`img`);
 
-  pinButton.setAttribute('style', `left: ${data.location.x - pinButton.offsetWidth}px; top: ${data.location.y - pinButton.offsetHeight}px;`);
-  pinPhoto.setAttribute('src', `${data.offer.photos}`);
-  pinPhoto.setAttribute('alt', `${data.offer.title}`);
+  pinButton.setAttribute(`style`, `left: ${data.location.x - pinButton.offsetWidth}px; top: ${data.location.y - pinButton.offsetHeight}px;`);
+  pinPhoto.setAttribute(`src`, `${data.offer.photos}`);
+  pinPhoto.setAttribute(`alt`, `${data.offer.title}`);
 
   return pinElement;
 };
